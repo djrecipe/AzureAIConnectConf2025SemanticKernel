01864d70602f48241350a5f06975a7c1b730da25
Justin Pierce <brkfstmnchr@gmail.com>
2025-03-06 11:12:18 +0700

    pretty basic example
    pretty basic example

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instructions
+1. Set-up vector database
+2. Build and run IronPdf.AI.POC.sln
+
+# Setting-Up Vector Database
+1. `git clone https://github.com/chroma-core/chroma.git`
+2. `cd chroma`
+3. `docker-compose up --build`
---
+++ SemanticKernelExampleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.Chroma;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Plugins.Memory;
+
+// SETUP AI
+var azureEndpoint = "https://EXAMPLE.openai.azure.com/";
+var apiKey = "abc123";
+var builder = Kernel.CreateBuilder()
+                    .AddAzureOpenAITextEmbeddingGeneration(
+                        "example-embedded-deployment-name", azureEndpoint, apiKey)
+                    .AddAzureOpenAIChatCompletion(
+                        "example-chat-deployment-name", azureEndpoint, apiKey);
+var kernel = builder.Build();
+
+// SETUP MEMORY
+var memory_builder = new MemoryBuilder()
+                        .WithMemoryStore(new VolatileMemoryStore())
+                        .WithAzureOpenAITextEmbeddingGeneration(
+                            "example-embedded-deployment-name", azureEndpoint, apiKey);
+
+var memory = memory_builder.Build();
